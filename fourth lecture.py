68b910886ea16eb919ebbd17341e809c13e5cc56
RobotComp.ru <maksimum20000@mail.ru>
2025-11-19 20:45:46 +0300

    completed the task for the fourth lecture

--- fourth lecture.py
+++ fourth lecture.py
@@ -1,3 +1,30 @@
+#Задача 1
+#Дан список: fruits = ["apple", "banana", "cherry"]
+#Добавьте элемент "orange" в конец этого списка.
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+
+#Задача 2
+#Дан список: numbers = [10, 20, 30, 40, 50, 60, 70]
+#Используя срезы, получите из него список [30, 40, 50].
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[2:5])
+
+#Задача 3
+#Дан список: colors = ["red", "blue", "green"]
+#Замените второй элемент (с индексом 1) на строку "yellow".
+colors = ["red", "blue", "green"]
+colors[1] = "yellow"
+print(colors)
+
+#Задача 4
+#Дан список: letters = ['a', 'b', 'd', 'e']
+#Добавьте элемент 'c' в список так, чтобы он оказался между 'b' и 'd' (т.е. на позицию с индексом 2).
+letters = ['a', 'b', 'd', 'e']
+letters.insert(2, "c")
+print(letters)
+
 #Задача 5
 #Дан список: items = ['book', 'pen', 'pencil', 'eraser', 'ruler']
 #Удалите элемент 'pencil' из списка, зная его значение.
